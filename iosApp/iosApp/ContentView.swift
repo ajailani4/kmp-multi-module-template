1b4255c8e1bc56ad771e46518ac238170f7d16ab
Ahmad Jailani <ahmadjailani604@gmail.com>
2025-03-03 13:52:49 +0700

    Add item detail screen on iOS app

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,22 +1,8 @@
 import SwiftUI
+import FeatureOne
 
 struct ContentView: View {
-    @State private var showContent = false
     var body: some View {
-        VStack {
-            Button("Click me!") {
-                withAnimation {
-                    showContent = !showContent
-                }
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        ItemDetailScreen()
     }
 }
---
+++ presentation-ios/FeatureOne/FeatureOne/Screen/ItemDetail/ItemDetailUiState.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Shared
+
+struct ItemDetailUiState {
+    var isLoading: Bool = false
+    var item: EntityExampleItem? = nil
+    var errorMessage: String? = nil
+}
